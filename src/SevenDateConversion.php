f66a1d90e516e59e28f23d7a93cb974c120056cb
Roger Pence <roger.pence@gmail.com>
2015-09-04 01:05:18 -0500

    Add PHP-codesniffer

--- src/SevenDateConversion.php
+++ src/SevenDateConversion.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App;
+
 use \DateTime;
 use \InvalidArgumentException;
 
@@ -14,11 +15,11 @@ class SevenDateConversion
     {
         $sevenDate = sprintf("%07d", $sourceDate);
 
-        if (!preg_match("/^(0|1)\d{6}$/",$sevenDate)) {
+        if (!preg_match("/^(0|1)\d{6}$/", $sevenDate)) {
             throw new InvalidArgumentException('seven-digit date not valid');
         }
 
-        $c = substr($sevenDate,0,1);
+        $c  = substr($sevenDate, 0, 1);
         $yy = substr($sevenDate, 1, 2);
         $mm = substr($sevenDate, 3, 2);
         $dd = substr($sevenDate, 5, 2);
